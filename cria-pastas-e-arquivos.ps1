130ee3ff4e0b29bbf4dd6a5f224593d27c451462
Gabriel Luiz <gabrielluiz@msn.com>
2022-04-19 22:05:49 -0300

    Add files via upload

---
+++ cria-pastas-e-arquivos.ps1
@@ -0,0 +1,25 @@
+﻿## Script de criação de pasta e arquivo de texto baseado na data - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+# Busca a data.
+
+$data = Get-Date -Format "MM-dd-yyyy-HH-mm"
+$nome = “$data”
+
+
+# Cria uma pasta com o nome da data.
+
+New-Item -Path C:\"$data" -ItemType Directory
+
+
+# Cria um arquivo de texto vazio com o nome da data. 
+
+New-Item -Path C:\"$data"\"$data".txt -ItemType File
+
+
+<#
+
+Observação: Este script foi criado apenas para testar o Agendador de Tarefas do Windows utilizando a conta de serviço gerenciado de grupo (gMSA).
+
+
+#>
